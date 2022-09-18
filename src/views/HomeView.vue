11b055c279d4abb96638edf5ac67413a6d276302
shellew <nishioka2013_1124@yahoo.co.jp>
2022-09-18 17:10:54 +0900

    create_register_page 1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 import Sidebar from '../components/items/Sidebar.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
     <Sidebar />
   </main>
 </template>
---
+++ src/views/RegisterPage.vue
@@ -0,0 +1,9 @@
+<script setup>
+    import Sidebar from '../components/items/Sidebar.vue'
+</script>
+
+<template>
+    <main>
+        <Sidebar />
+    </main>
+</template>
