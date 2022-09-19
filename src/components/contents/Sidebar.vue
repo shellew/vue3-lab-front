414779a22c9854cd9e523acd722a5507c79bb16e
shellew <nishioka2013_1124@yahoo.co.jp>
2022-09-20 08:14:09 +0900

    サイドバールーティング設定

--- src/components/contents/Sidebar.vue
+++ src/components/contents/Sidebar.vue
@@ -15,22 +15,22 @@
           </template>
           <el-menu-item-group>
             <el-menu-item index="1-1">
-              <a href="#">読書中</a>
+              <RouterLink to="/list">読書中</RouterLink>
             </el-menu-item>
             <el-menu-item index="1-2">
-              <a href="#">積読本</a>
+              <RouterLink to="/list">積読本</RouterLink>
             </el-menu-item>
             <el-menu-item index="1-3">
-              <a href="#">読んだ</a>
+              <RouterLink to="/list">読んだ</RouterLink>
             </el-menu-item>
             <el-menu-item index="1-4">
-              <a href="#">読みたい</a>
+              <RouterLink to="/list">読みたい</RouterLink>
             </el-menu-item>
           </el-menu-item-group>
         </el-sub-menu>
         <el-menu-item index="1-5">
           <el-icon><document /></el-icon>
-          <a href="#">新しい本を登録</a>
+          <RouterLink to="/register">新しい本を登録</RouterLink>
         </el-menu-item>
         <el-menu-item index="1-6">
           <el-icon><setting /></el-icon>
@@ -42,6 +42,7 @@
 </template>
 
 <script lang="ts" setup>
+import { RouterLink, RouterView } from 'vue-router'
 import {
   Document,
   Menu as IconMenu,
